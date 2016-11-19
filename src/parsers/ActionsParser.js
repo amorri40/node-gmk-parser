01dfd75bd6b7c2bb26c541a0c26e607c689fd2b6
Alasdair Morrison <amorri40@gmail.com>
2016-11-19 15:54:54 +0000

    Added Timelines and Actions Parsing

---
+++ src/parsers/ActionsParser.js
@@ -0,0 +1,45 @@
+var Parser = require("binary-parser").Parser;
+var GMKZlib = require("./GMKZlib.js");
+var VersionCheck = require("../util/VersionChecks");
+var Common = require("../util/CommonTypes");
+var CommonFunctions = require("../util/CommonFunctions");
+
+var ArgumentParser = Parser.start()
+                .endianess('little')
+                //
+                // # TODO weird argnumber vs actual arg number
+                //
+                .nest('value',{type:Common.GMString})
+
+
+var ArgumentKindParser = Parser.start()
+                .endianess('little')
+                .int32('kind')
+
+var ActionParser = Parser.start()
+                .endianess('little')
+                .int32('skip')
+                .int32('libraryId')
+                .int32('actionId')
+                .int32('actionKind')
+                .int32('allowRelative')
+                .int32('question')
+                .int32('canApplyTo')
+                .int32('execType')
+                .nest('ExecFunction',{type:Common.GMString})
+                .nest('ExecCode',{type:Common.GMString})
+                .int32('argumentNumber')
+                .int32('argumentKindNumber')
+                .array('argumentKinds',{type:ArgumentKindParser,length:'argumentKindNumber'})
+                .int32('appliesTo')
+                .int32('relative')
+                .int32('ActualNumberOfArguments')
+                .array('arguments',{type:ArgumentParser,length:'ActualNumberOfArguments'})
+                .int32('not')
+
+
+module.exports.ActionsParser = Parser.start()
+                .endianess('little')
+                .uint32('version')
+                .int32('numberOfActions')
+                .array('actions',{type:ActionParser,length:'numberOfActions'})
---
+++ src/parsers/TimelinesParser.js
@@ -0,0 +1,32 @@
+var Parser = require("binary-parser").Parser;
+var GMKZlib = require("./GMKZlib.js");
+var VersionCheck = require("../util/VersionChecks");
+var Common = require("../util/CommonTypes");
+var CommonFunctions = require("../util/CommonFunctions");
+var Actions = require("./ActionsParser")
+
+var ResourceName = "Timeline"
+var ResourcesName = ResourceName+"s";
+var GMResourceName = "GM"+ResourceName;
+var GMResourcesName = GMResourceName+"s";
+
+
+var MomentsParser = Parser.start()
+                .endianess('little')
+                .int32('stepNumber')
+                .nest('actions',{type:Actions.ActionsParser})
+
+module.exports[GMResourceName+"Data"] = Parser.start()
+                .endianess('little')
+                .nest('Name',{type:Common.GMString})
+                .nest('gm8',{type:Common.GM8LastChanged})
+                .uint32('version')
+                //
+                // # TODO : Handle version 440 Data files
+                //
+                .int32('numberOfMoments')
+                .array('moments',{type:MomentsParser,length:'numberOfMoments'})
+                .nest('stdout', {type: Common.NewStdoutMessage(ResourcesName)})
+
+module.exports[GMResourceName] = Common.NewValidCheckerForGMResource(module.exports[GMResourceName+"Data"]);
+module.exports[GMResourcesName] = Common.NewGMResource(ResourceName, ResourcesName, GMResourceName, module.exports[GMResourceName]);
